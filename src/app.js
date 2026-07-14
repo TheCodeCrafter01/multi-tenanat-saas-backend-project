c9ef2dc710ccd0345f947eed72ed8f5136e312fe
Indra Mohan Singh <145895827+TheCodeCrafter01@users.noreply.github.com>
2026-07-14 23:30:41 +0530

    Fix route filename casing

--- src/app.js
+++ src/app.js
@@ -1,23 +1,14 @@
-
 const express = require("express");
 const app = express();
+
 app.use(express.json());
-const tenantRoutes = require("./routes/tenantroutes");
 
-
-// Database connect
-
-
-// Create an instance of Express
-
-
-const homeRoutes = require("./routes/homeroutes");
-const userRoutes = require("./routes/userroutes");
+const homeRoutes = require("./routes/homeRoutes");
+const tenantRoutes = require("./routes/tenantRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 app.use("/", homeRoutes);
 app.use("/users", userRoutes);
 app.use("/tenants", tenantRoutes);
-
-
 
 module.exports = app;
--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const router = express.Router();
 
 const { getHome } = require("../controllers/homeController");
